ccf60d1a0e01b0104fba0fae3f98324158984f93
Damilola Ekanola <114293301+damiekanola@users.noreply.github.com>
2023-01-26 16:42:46 +0100

    Delete rearrange alphabetically.py

--- rearrange alphabetically.py
+++
@@ -1,7 +0,0 @@
-animals = ['dog','cat','elephant','zebra','bee','fish','dolphin']
-animals.sort(reverse=True)
-print(animals)
-
-animalslist = ['dog','cat','elephant','zebra','bee','fish','dolphin']
-animalslist.sort()
-print(animalslist)
