6ee2fbdb0a3cc41fa09e9528a94e8b8161ddd4d8
Damilola Ekanola <114293301+damiekanola@users.noreply.github.com>
2023-01-26 14:33:43 +0100

    Add files via upload

---
+++ rearrange alphabetically.py
@@ -0,0 +1,7 @@
+animals = ['dog','cat','elephant','zebra','bee','fish','dolphin']
+animals.sort(reverse=True)
+print(animals)
+
+animalslist = ['dog','cat','elephant','zebra','bee','fish','dolphin']
+animalslist.sort()
+print(animalslist)
